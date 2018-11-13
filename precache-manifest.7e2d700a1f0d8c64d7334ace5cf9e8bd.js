7d51b4e613d720e9d03881d04ecbc96704ebfc16
Taras Danyliuk <tarik.danylyuk@gmail.com>
2018-11-13 21:27:12 +0200

    ready for deploy

---
+++ precache-manifest.7e2d700a1f0d8c64d7334ace5cf9e8bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52384e4cab164a1c3c3b",
+    "url": "/static/js/main.52384e4c.chunk.js"
+  },
+  {
+    "revision": "b2ff80dc69a3f75cabd9",
+    "url": "/static/js/1.b2ff80dc.chunk.js"
+  },
+  {
+    "revision": "52384e4cab164a1c3c3b",
+    "url": "/static/css/main.fd1e5844.chunk.css"
+  },
+  {
+    "revision": "cdb89e380db161aadaaace044e1a0357",
+    "url": "/index.html"
+  }
+];
